e076739acf7ab9d9937f744642d9f449672d3cc4
AKGEC <utkarsh2110024@akgec.ac.in>
2023-04-05 15:44:50 +0530

    added assets

--- lib/utils/app_image.dart
+++ lib/utils/app_image.dart
@@ -1,26 +1,26 @@
 class AppImages {
-  static final String menuSvg = "assets/menu.svg";
-  static final String user = "assets/user.png";
+  static final String menuSvg = "assets/images/menu.svg";
+  static final String user = "assets/images/user.png";
 
   // onboarding image
-  static final String onboarding1 = "assets/onboarding1.png";
-  static final String onboarding2 = "assets/onboarding2.png";
+  static final String onboarding1 = "assets/images/onboarding1.png";
+  static final String onboarding2 = "assets/images/onboarding2.png";
 
-  static final String bg1Png = "assets/bg1.png";
-  static final String bg2Png = "assets/bg2.png";
-  static final String bg3Png = "assets/bg3.png";
+  static final String bg1Png = "assets/images/bg1.png";
+  static final String bg2Png = "assets/images/bg2.png";
+  static final String bg3Png = "assets/images/bg3.png";
 
   // bottom menu
-  static final String menu_disc = "assets/menu_disc.svg";
-  static final String menu_home = "assets/menu_home.svg";
-  static final String menu_moon = "assets/menu_moon.svg";
-  static final String menu_music = "assets/menu_music.svg";
+  static final String menu_disc = "assets/images/menu_disc.svg";
+  static final String menu_home = "assets/images/menu_home.svg";
+  static final String menu_moon = "assets/images/menu_moon.svg";
+  static final String menu_music = "assets/images/menu_music.svg";
 
   // emojis
-  static final String angryPng = "assets/angry.png";
-  static final String anxiousPng = "assets/anxious.png";
-  static final String cryingPng = "assets/crying.png";
-  static final String grinningPng = "assets/grinning.png";
-  static final String irritatedPng = "assets/irritated.png";
-  static final String sleepyPng = "assets/sleepy.png";
+  static final String angryPng = "assets/images/angry.png";
+  static final String anxiousPng = "assets/images/anxious.png";
+  static final String cryingPng = "assets/images/crying.png";
+  static final String grinningPng = "assets/images/grinning.png";
+  static final String irritatedPng = "assets/images/irritated.png";
+  static final String sleepyPng = "assets/images/sleepy.png";
 }
